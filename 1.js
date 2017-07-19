f4b93a9bc961a144da3d934c913410cda799dae4
shawnyu189 <yushuai1011@live.com>
2017-07-19 13:25:04 +0800

    lion3D

--- 1.js
+++
@@ -1,9 +0,0 @@
-var oDiv = document.getElementsByTagName('div')[0];
-
-document.onmousemove = function(ev){
-
-	oDiv.style.left = ev.clientX - 100 + 'px';
-
-	oDiv.style.top = ev.clientY - 100 + 'px';
-
-}
